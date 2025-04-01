cb6771e9c7b333dfc38b5276798e4138a18c1c4f
vinicius_lozano <viniciuslozanodeolivera@hotmail.com>
2025-04-01 15:58:17 -0700

    mvp

--- filemanager/forms.py
+++ filemanager/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Docs
 
 class DocumentForm(forms.ModelForm):
+    custom_name = forms.CharField(max_length=255, required=False, label="Nome Customizado", widget=forms.TextInput(attrs={'placeholder': 'Nome do arquivo'}))
+
     class Meta:
         model = Docs
-        fields = ['file', 'onedrive_link']
+        fields = ['file', 'onedrive_link', 'custom_name']
