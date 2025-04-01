6b8fa0c909eba8fed8131d2c274234db9a5ff83d
vinicius_lozano <viniciuslozanodeolivera@hotmail.com>
2025-04-01 12:56:25 -0700

    css e imgs

--- setup/settings.py
+++ setup/settings.py
@@ -105,6 +105,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 LOGIN_REDIRECT_URL = 'task-list'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
